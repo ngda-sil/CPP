6c3afaf425faacb1c896aa6066307b9ad07e8a43
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2023-02-07 23:11:13 +0100

    C00 : Ex00 : Changed while loop in for loop

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -3,20 +3,13 @@
 
 int	main(int ac, char **av)
 {
-	int	i = 1;
-	int	j;
-	
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	while (i < ac)
+	for (int i = 1; i < ac; i++)
 	{
-		j = 0;
-		while (av[i][j])
-		{
+		for (int j = 0; av[i][j]; j++)
 			std::cout << (char)toupper(av[i][j]);
-			j++;
-		}
-		i++;
+		std::cout << " ";
 	}
 	std::endl (std::cout);
 	return(0);
