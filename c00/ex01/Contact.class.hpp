b18ddcee476663c20421357ff3ed39dd9f200eb6
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2023-02-07 19:20:37 +0100

    C00: Ex01 :Started PhoneBook

--- c00/ex01/Contact.class.hpp
+++ c00/ex01/Contact.class.hpp
@@ -1,10 +1,17 @@
 #ifndef CONTACT_CLASS_H
 # define CONTACT_CLASS_H
-
+# include <string>
 class Contact{
 
 public:
 	Contact(void);
 	~Contact(void);
-};
+
+	std::string	FirstName;
+	char	LastName[100];
+	char	NickName[100];
+	int		PhoneNumber;
+	char	DarkestSecret[100];
+
+	};
 #endif
--- c00/ex01/PhoneBook.class.hpp
+++ c00/ex01/PhoneBook.class.hpp
@@ -1,10 +1,18 @@
 #ifndef PHONEBOOK_CLASS_H
 # define PHONEBOOK_CLASS_H
 
+# include "Contact.class.hpp"
+
 class PhoneBook {
 
 public:
 	PhoneBook(void);
 	~PhoneBook(void);
+
+	Contact	tab[8];
+	int		i;
+
+	//Contact*	initCon(void);
+	void		addContact(int i);
 };
 #endif
