28a0ae0911f19c4ad38eeda3a869b26ecda06c17
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2023-02-10 13:45:10 +0100

    C01 : Ex03 : started

---
+++ c01/ex03/Weapon.hpp
@@ -0,0 +1,17 @@
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+
+class Weapon
+{
+	public:
+		Weapon();
+		~Weapon();
+		void setType(std::string type)
+
+	private:
+		sts::string	_type;
+};
+
+#endif
