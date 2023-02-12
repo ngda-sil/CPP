e83881eb7b0c774e14113ecfe6404e5383026557
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2023-02-12 22:03:31 +0100

    C00 : Ex00 : clean

--- c00/ex00/main.cpp
+++ c00/ex00/main.cpp
@@ -6,11 +6,8 @@ int	main(int ac, char **av)
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; i++)
-	{
 		for (int j = 0; av[i][j]; j++)
 			std::cout << (char)toupper(av[i][j]);
-		std::cout << " ";
-	}
 	std::endl (std::cout);
 	return(0);
 }
