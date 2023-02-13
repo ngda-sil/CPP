6d86c2a40d1e8f4b97c347878917ec9f5c8e31b5
Tracy Da Silva <ngda-sil@student.42lausanne.ch>
2023-02-13 20:36:21 +0100

    fixed Makefile

--- c02/ex00/Fixed.cpp
+++ c02/ex00/Fixed.cpp
@@ -1,9 +1,8 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed() : _RawBits(0)
+Fixed::Fixed() : _rawbits(0)
 {
 	_Fraction = 8;
-	return;
 }
 
 Fixed::Fixed(Fixed const & src)
@@ -23,10 +22,10 @@ Fixed::~Fixed()
 
 int	Fixed::getRawBits(void) const
 {
-	return (this->_RawBits);
+	return (this->_rawbits);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	this->_RawBits = raw;
+	this->_rawits = raw;
 }
